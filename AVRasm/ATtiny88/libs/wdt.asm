69cc6f39886c4f4a0dbf4cc201b80f44d466c734
ogneyar <ya13th@mail.ru>
2023-02-15 10:08:51 +0300

    eeee

--- AVRasm/ATtiny88/libs/wdt.asm
+++ AVRasm/ATtiny88/libs/wdt.asm
@@ -2,47 +2,55 @@
 #ifndef _WDT_ASM_
 #define _WDT_ASM_
 
+#include "../libs/macro.inc"    ; подключение файла 'макросов'
+
 ;=================================================
 WDT_Init:   
-    ; Turn off global interrupt
-    cli
-    ; Reset Watchdog Timer
-    wdr
+    push    R16
+    push    R17
+    mIN     R17, SREG    
+    CLI ; Turn off global interrupt
+    WDR ; Reset Watchdog Timer
     ; Start timed sequence
-    LDS r16, WDTCSR
-    ori r16, (1 << WDCE) | (1 << WDE) | (1 << WDIE)
-    STS WDTCSR, r16
+    mIN     R16, WDTCSR
+    ORI     R16, (1 << WDCE) | (1 << WDE) | (1 << WDIE)
+    mOUT    WDTCSR, R16
     ; -- Got four cycles to set the new values from here -
     ; Set new prescaler(time-out) value = 64K cycles (~0.5 s)
-    ; ldi r16, (1 << WDP2) | (1 << WDP0) ; 0,5 секунды
-    ldi r16, (1 << WDP2) | (1 << WDP1) | (1 << WDP0) ; 2 секунды
-    ; ldi r16, (1 << WDP3) | (1 << WDP0) ; 8 секунд
-    ori r16, (1 << WDE) | (1 << WDIE) 
-    STS WDTCSR, r16
+    ; ldi   R16, (1 << WDP2) | (1 << WDP0) ; 0,5 секунды
+    LDI     R16, (1 << WDP2) | (1 << WDP1) | (1 << WDP0) ; 2 секунды
+    ; ldi   R16, (1 << WDP3) | (1 << WDP0) ; 8 секунд
+    ORI     R16, (1 << WDE) | (1 << WDIE) 
+    mOUT    WDTCSR, R16
     ; -- Finished setting new values, used 2 cycles -
     ; Turn on global interrupt
-    sei
+    mOUT    SREG, R17 ; sei
+    pop     R17
+    pop     R16
 ret
 
 WDT_off:
-    ; Turn off global interrupt
-    cli
-    ; Reset Watchdog Timer
-    wdr
+    push    R16
+    push    R17
+    mIN     R17, SREG    
+    CLI ; Turn off global interrupt
+    WDR ; Reset Watchdog Timer
     ; Clear WDRF in MCUSR
-    in r16, MCUSR
-    andi r16, (0xff & (0<<WDRF))
-    out MCUSR, r16
+    mIN     R16, MCUSR
+    ANDI    R16, (0xff & (0 << WDRF))
+    mOUT    MCUSR, R16
     ; Write logical one to WDCE and WDE
     ; Keep old prescaler setting to prevent unintentional time-out
-    lds r16, WDTCSR
-    ori r16, (1<<WDCE) | (1<<WDE)
-    sts WDTCSR, r16
+    mIN     R16, WDTCSR
+    ORI     R16, (1 << WDCE) | (1 << WDE)
+    mOUT    WDTCSR, R16
     ; Turn off WDT
-    ldi r16, (0<<WDE)
-    sts WDTCSR, r16
+    LDI     R16, (0 << WDE)
+    mOUT    WDTCSR, R16
     ; Turn on global interrupt
-    sei
+    mOUT    SREG, R17 ; sei
+    pop     R17
+    pop     R16
 ret
 ;=================================================
 
